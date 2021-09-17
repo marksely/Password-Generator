149c0c9903a09ba50db80fcb022d1e4360f0b35d
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-09-17 18:46:39 -0400

    password now appears

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ var numbers = [0,1,2,3,4,5,6,7,8,9];
 var letters = ['a','b','c','d','e', 'f','g','h','i','j','k','l','m'];
 var upperCaseLetters = ['A','B','C','D','E', 'F','H','I','J','K','L','M'];
 var specialCharacters = ['!','#','&','@','+','/','?',';',']',')','+','(','.','"'];
-var generatedPassword =["loading"];
-//var generatedCharacters = [];
-//console.log(generatedCharacters);
+let emptyArr = [];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -19,44 +18,34 @@ function generatePassword() {
   
   var lowerCase = window.confirm("Would you like the password to contain lowercase letters?");
   if(lowerCase) {
-    var genRandomLower = function() {
-      var letterIndex = Math.floor(Math.random() * letters.length);
-      var randomLetter = letters[letterIndex];
-      // generatedCharacters.push(randomLetter);
-    }
-  }
+    var letterIndex = Math.floor(Math.random() * letters.length);
+    var randomLetter = letters[letterIndex];
+    emptyArr.push(randomLetter);
+   }
 
   var upperCase = window.confirm("Would you like the password to contain uppercase letters?");
   if(upperCase) {
-    var genRandomUpper = function() {
-      var upperCaseIndex = Math.floor(Math.random() * upperCaseLetters.length);
-      var randomUpper = upperCaseLetters[upperCaseIndex];
-      // generatedCharacters.push(randomUpper);
-    }
+    var upperCaseIndex = Math.floor(Math.random() * upperCaseLetters.length);
+    var randomUpper = upperCaseLetters[upperCaseIndex];
+    emptyArr.push(randomUpper);
   }
 
   var numeric = window.confirm("Would you like your password to contain numbers?");
   if(numeric) {
-    var genRandomNum = function() {
-      var numericIndex = Math.floor(Math.random() * numbers.length);
-      var randomNumber = numbers[numericIndex];
-      // generatedCharacters.push(randomNumber);
-    }
+    var numericIndex = Math.floor(Math.random() * numbers.length);
+    var randomNumber = numbers[numericIndex];
+    emptyArr.push(randomNumber);
   } 
 
   var special = window.confirm("Would you like your password to contain special characters?");
   if(special) {
-    var genSpecial = function() {
-      var specialIndex = Math.floor(Math.random() * specialCharacters.length);
-      var randomSpecial = specialCharacters[specialIndex];
-      // generatedCharacters.push(randomSpecial);
-    } 
+    var specialIndex = Math.floor(Math.random() * specialCharacters.length);
+    var randomSpecial = specialCharacters[specialIndex]; 
+    emptyArr.push(randomSpecial);
   }
-  // generate the password 
   
-
   //displays the password
-  return 'Loading';
+  return emptyArr.join([]) + emptyArr.join([]) + emptyArr.join([]);
 }
 
 // Write password to the #password input
